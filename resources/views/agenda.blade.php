c28e9afcb903765505c8b94c6b221b51dbf58414
Heitor Berischmeier <111311203+eooheitor@users.noreply.github.com>
2024-05-31 11:00:15 -0300

    Alterando o front

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Agende seu horário') }}
         </h2>
     </x-slot>
 
--- resources/views/agendamentos.blade.php
+++ resources/views/agendamentos.blade.php
@@ -1,42 +1,52 @@
-@extends('index')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Agendamentos') }}
+        </h2>
+    </x-slot>
 
-@section('title', 'Agendamentos')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="title text-center mb-4">
+                                    <h3>Mesas Agendadas</h3>
+                                </div>
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Nome</th>
+                                            <th scope="col">Telefone</th>
+                                            <th scope="col">Data</th>
+                                            <th scope="col">Hora</th>
+                                            <th scope="col">Mesa</th>
+                                            <th scope="col">Número de pessoas</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="title text-center mb-4">
-                    <h3>Mesas Agendadas</h3>
+                                        @foreach($agendamentos as $agendamento)
+                                        <tr>
+                                            <th scope="row">{{$agendamento->id}}</th>
+                                            <td>{{$agendamento->nome}}</td>
+                                            <td>{{$agendamento->telefone}}</td>
+                                            <td>{{date('d-m-Y', strtotime($agendamento->data));}}</td>
+                                            <td>{{ date("H:i", strtotime($agendamento->hora)) }}</td>
+                                            <td>{{$agendamento->mesa}}</td>
+                                            <td>{{$agendamento->nropessoas}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Telefone</th>
-                            <th scope="col">Data</th>
-                            <th scope="col">Hora</th>
-                            <th scope="col">Mesa</th>
-                            <th scope="col">Número de pessoas</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    
-                      @foreach($agendamentos as $agendamento)
-                        <tr>
-                            <th scope="row">{{$agendamento->id}}</th>
-                            <td>{{$agendamento->nome}}</td>
-                            <td>{{$agendamento->telefone}}</td>
-                            <td>{{date('d-m-Y', strtotime($agendamento->data));}}</td>
-                            <td>{{ date("H:i", strtotime($agendamento->hora)) }}</td>
-                            <td>{{$agendamento->mesa}}</td>
-                            <td>{{$agendamento->nropessoas}}</td>
-                        </tr>
-                      @endforeach  
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
-@endsection
+</x-app-layout>
